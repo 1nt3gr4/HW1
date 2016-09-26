63647a9f4189b63233f2d8ba64aace0b6a9ffada
1nt3gr4 <vadem.gati@gmail.com>
2016-09-26 12:21:30 +0300

    Add files via upload

---
+++ 1.5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char x1 = (char)Console.Read();
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            char x2 = (char)Console.Read();
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            if (Math.Abs(x1 - x2) == Math.Abs(y1 - y2))
+                Console.WriteLine("Данные клетки лежат на одной диагонали.");
+            else
+                Console.WriteLine("Данные клетки не лежат на одной диагонали.");
+        }
+    }
+}
