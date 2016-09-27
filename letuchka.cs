3829af56e01a06591aabc34a7d6d282935401f0d
1nt3gr4 <vadem.gati@gmail.com>
2016-09-27 13:06:12 +0300

    Add files via upload

---
+++ letuchka.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace practice
+{
+    class letuchka
+    {
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            if (((x >= 0) && (y >= 0) && (x <= 1) && (y <= 1)) || ((x >= 0) && (y <= 0) && (y >= -x * x)))
+                Console.WriteLine("Принадлежит");
+            else
+                Console.WriteLine("Не принадлежит");
+        }
+    }
+}
